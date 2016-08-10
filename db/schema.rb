c5869f03fd5bc5f3f7a3686541d8d769c3106020
Jonathan Cho <jonofcho@gmail.com>
2016-08-10 14:34:18 -0700

    add Avatar to Organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808210727) do
+ActiveRecord::Schema.define(version: 20160810213301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,12 @@ ActiveRecord::Schema.define(version: 20160808210727) do
     t.text     "description"
     t.integer  "school_admin"
     t.text     "school_origin"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "organizations", ["user_id"], name: "index_organizations_on_user_id", using: :btree
