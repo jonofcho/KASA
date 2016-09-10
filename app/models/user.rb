dc1a1b0d996783fe9ca0d9af936bc19cb095d7e1
Jonathan Cho <jonofcho@gmail.com>
2016-09-10 02:14:36 -0700

    update users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,23 @@ class User < ActiveRecord::Base
   # validates :first_name, :presence => true
   # validates :last_name, :presence => true
   validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
-  validates :password, :presence => true, :on => :create
+  validates :password, :presence => true
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-
-  belongs_to :organization
-  belongs_to :staff
-
+  has_many :skills, dependent: :destroy
+  has_many :project_users, dependent: :destroy
+  has_many :projects, through: :project_users
   has_many :posts, dependent: :destroy
 
-  def self.search(search)
-    where("first_name || ' ' || last_name like :s", :s => "%#{search}")
-  end
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" },
+      :default_url => "/images/:style/missing.png",
+      :url  => ":s3_domain_url",
+      :path => "public/avatars/:id/:style_:basename.:extension",
+      :storage => :fog,
+      :fog_credentials => {
+          provider: 'AWS',
+          aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+          aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+      },
+      fog_directory: ENV["FOG_DIRECTORY"]
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
