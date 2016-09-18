aa9d66cfdf5810e881b9257c4c6a5448609310a4
Jonathan Cho <jonofcho@gmail.com>
2016-09-18 16:01:44 -0700

    add fixed message-to-org button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'messages/index'
+
+  get 'messages/create'
+
+  get 'messages/show'
+
+  get 'messages/edit'
+
+  get 'messages/update'
+
+  get 'messages/destroy'
+
   get 'logistics/edit'
 
   get 'logistics/update'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912220454) do
+ActiveRecord::Schema.define(version: 20160918223818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 20160912220454) do
   end
 
   add_index "logistics", ["organization_id"], name: "index_logistics_on_organization_id", using: :btree
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.string   "content"
+    t.datetime "timestamp"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.integer  "user_id"
