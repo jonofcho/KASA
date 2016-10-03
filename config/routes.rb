90d803d5b3c5e4fa6e09c3c664718e327ceb99d4
Jonathan Cho <jonofcho@gmail.com>
2016-10-02 19:36:41 -0700

    continuation

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'messages/index'
-
-  get 'messages/create'
-
-  get 'messages/show'
-
-  get 'messages/edit'
-
-  get 'messages/update'
-
-  get 'messages/destroy'
-
-  get 'logistics/edit'
-
-  get 'logistics/update'
-
   devise_scope :user do
     root to: "devise/sessions#new"
   end
